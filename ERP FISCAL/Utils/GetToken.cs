6f5057fdc571de993c7b1cddc4d32059fcb79256
Lucas Pires <lucas.pires@solfarma.com.br>
2025-10-29 15:19:00 -0300

    Token pela decima vez

--- ERP FISCAL/Utils/GetToken.cs
+++ ERP FISCAL/Utils/GetToken.cs
@@ -9,39 +9,41 @@ using System.Threading.Tasks;
 
 namespace ERP_FISCAL.Utils
 {
-    public class GetToken
-    {
-        public async Task<string> ObterTokenAsync()
+        public class GetToken
         {
-            string token = null;
-            var conexaoBanco = new ConexaoBancoDeDadosZanup();
-
-            try
+            public async Task<string> ObterTokenAsync()
             {
-                using (SqlConnection conn = conexaoBanco.AbrirConexao())
-                using (SqlCommand cmd = new SqlCommand(@"
-                                                        SELECT 
-                                                            TOP 1 tblAutBling.Token
-                                                        FROM 
-                                                            Zanup.dbo.tblAutenticacaoBling tblAutBling (NOLOCK)
-                                                        WHERE 
-                                                            tblAutBling.Ativo = 1
-                                                       ", conn))
+                string token = null;
+                var conexaoBanco = new ConexaoBancoDeDadosZanup();
+
+                try
                 {
-                    cmd.CommandType = CommandType.Text;
+                    using (SqlConnection conn = conexaoBanco.AbrirConexao())
+                    using (SqlCommand cmd = new SqlCommand(@"
+                                                            SELECT 
+                                                                TOP 1 tblAutBling.Token
+                                                            FROM   
+                                                                Zanup.dbo.tblAutenticacaoBling tblAutBling (NOLOCK)
+                                                            WHERE 
+                                                                tblAutBling.Ativo = 1
+                                                             ", conn
+                                                            ))
+                    {
+                        cmd.CommandType = CommandType.Text;
 
-                    var result = await cmd.ExecuteScalarAsync();
+                        var result = await cmd.ExecuteScalarAsync();
 
-                    if (result != null && result != DBNull.Value)
-                        token = result.ToString();
+                        if (result != null && result != DBNull.Value)
+                            token = result.ToString();
+                    }
                 }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Erro ao consultar token: " + ex.Message, ex);
-            }
+                catch (Exception ex)
+                {
+                    throw new Exception("Erro ao consultar token: " + ex.Message, ex);
+                }
 
-            return token;
+                return token;
+            }
         }
-    }
+
 }
