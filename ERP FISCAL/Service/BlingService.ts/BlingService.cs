185201cb9cec72c6a2b4865c6ed3d22b67a3af77
Rudinei <costaaugusto177@outlook.com>
2025-10-29 15:00:39 -0300

    criando função de tranformar datarow em objeto para requisição

---
+++ ERP FISCAL/Service/BlingService.ts/BlingService.cs
@@ -0,0 +1,34 @@
+﻿using ERP_FISCAL.Service.BlingService.ts;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP_FISCAL.service
+{
+    public class BlingService
+    {
+        public async Task<string> CriarNotaAsync(IDtoBlingNotaFiscal data)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", "c2b04a69023cd88cf12d6f188581f9f64414cebb");
+
+
+            string json = JsonConvert.SerializeObject(data);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync("https://api.bling.com.br/Api/v3/nfe", content);
+            var retorno = await response.Content.ReadAsStringAsync();
+
+            return $"Status: {(int)response.StatusCode} - {response.StatusCode}\r\n{retorno}";
+        }
+    }
+}
---
+++ ERP FISCAL/Service/BlingService.ts/DtoBlingNotaFiscal.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ERP_FISCAL.Service.BlingService.ts
+{
+    public interface IDtoBlingNotaFiscal
+    {
+        int Tipo { get; set; }
+        string Numero { get; set; }
+        DateTime DataOperacao { get; set; }
+        IContato Contato { get; set; }
+        List<IItens> Itens { get; set; }
+        string NaturezaOperacao { get; set; }
+    }
+
+    public class DtoBlingNotaFiscal : IDtoBlingNotaFiscal
+    {
+        [JsonProperty("tipo")]
+        public int Tipo { get; set; }
+
+        [JsonProperty("numero")]
+        public string Numero { get; set; }
+
+        [JsonProperty("dataOperacao")]
+        public DateTime DataOperacao { get; set; }
+
+        [JsonProperty("naturezaOperacao")]
+        public string NaturezaOperacao { get; set; }
+
+        [JsonProperty("contato")]
+        public IContato Contato { get; set; }
+
+        [JsonProperty("itens")]
+        public List<IItens> Itens { get; set; }
+    }
+
+    public interface IContato
+    {
+        string NumeroDocumento { get; set; }
+        string Nome { get; set; }
+    }
+
+    public class Contato : IContato
+    {
+        [JsonProperty("numeroDocumento")]
+        public string NumeroDocumento { get; set; }
+
+        [JsonProperty("nome")]
+        public string Nome { get; set; }
+    }
+
+    public interface IItens
+    {
+        string Codigo { get; set; }
+        string Descricao { get; set; }
+        string Unidade { get; set; }
+        int Quantidade { get; set; }
+        double Valor { get; set; }
+    }
+
+    public class Itens : IItens
+    {
+        [JsonProperty("codigo")]
+        public string Codigo { get; set; }
+
+        [JsonProperty("descricao")]
+        public string Descricao { get; set; }
+
+        [JsonProperty("unidade")]
+        public string Unidade { get; set; }
+
+        [JsonProperty("quantidade")]
+        public int Quantidade { get; set; }
+
+        [JsonProperty("valor")]
+        public double Valor { get; set; }
+    }
+}
