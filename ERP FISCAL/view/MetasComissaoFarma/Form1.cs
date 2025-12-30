6f60898829f2c0b46da114d2cc07cb437761c385
Rudinei <costaaugusto177@outlook.com>
2025-12-30 17:48:40 -0300

    ref

--- ERP FISCAL/view/MetasComissaoFarma/Form1.cs
+++ ERP FISCAL/view/MetasComissaoFarma/Form1.cs
@@ -36,5 +36,15 @@ namespace ERP_FISCAL.view.MetasComissaoFarma
         {
 
         }
+
+        private void btnExportar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
