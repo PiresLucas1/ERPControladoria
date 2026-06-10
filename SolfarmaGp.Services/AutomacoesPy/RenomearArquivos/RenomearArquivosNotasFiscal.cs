15d774498081ea83255dcdc4eebe8800f7fba232
Rudinei <costaaugusto177@outlook.com>
2026-06-10 18:04:51 -0300

    refactor

---
+++ SolfarmaGp.Services/AutomacoesPy/RenomearArquivos/RenomearArquivosNotasFiscal.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace SolfarmaGp.Services.AutomacoesPy.RenomearArquivos
+{
+    public class RenomearArquivosNotasFiscal
+    {
+        public void Execute(string pathFile, Action<string> onOutput) 
+        {
+            string pythonExe = "";
+            string script = "";
+
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = pythonExe,
+                Arguments = $"\"{script}\" \"{pathFile}\"",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,   // captura o print() do Python
+                RedirectStandardError = true,   // captura erros
+                CreateNoWindow = true    // não abre janela do terminal
+            };
+
+            using (Process process = Process.Start(psi))
+            {
+                string linha;
+                while ((linha = process.StandardOutput.ReadLine()) != null)
+                {
+                    onOutput(linha);
+                }
+                process.WaitForExit();
+            }
+        }
+
+    }
+}
---
+++ SolfarmaGp.UI/MenusUI/Fiscal/NovaPasta/ExcutaAutomacaoRenomearArquivos.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace SolfarmaGp.UI.MenusUI.Fiscal.NovaPasta
+{
+    partial class ExcutaAutomacaoRenomearArquivos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txbPath = new TextBox();
+            label1 = new Label();
+            btnExecutar = new Button();
+            btnCancelar = new Button();
+            btnAbrirPasta = new Button();
+            rtbInfo = new RichTextBox();
+            SuspendLayout();
+            // 
+            // txbPath
+            // 
+            txbPath.Location = new Point(12, 67);
+            txbPath.Name = "txbPath";
+            txbPath.PlaceholderText = "C://www/jhondoe/arquivos";
+            txbPath.Size = new Size(366, 27);
+            txbPath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(182, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Insira o caminho da pasta:";
+            // 
+            // btnExecutar
+            // 
+            btnExecutar.Location = new Point(500, 64);
+            btnExecutar.Name = "btnExecutar";
+            btnExecutar.Size = new Size(109, 30);
+            btnExecutar.TabIndex = 2;
+            btnExecutar.Text = "Executar";
+            btnExecutar.UseVisualStyleBackColor = true;
+            btnExecutar.Click += btnExecutar_Click;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(27, 408);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(109, 30);
+            btnCancelar.TabIndex = 3;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAbrirPasta
+            // 
+            btnAbrirPasta.Location = new Point(464, 408);
+            btnAbrirPasta.Name = "btnAbrirPasta";
+            btnAbrirPasta.Size = new Size(109, 30);
+            btnAbrirPasta.TabIndex = 4;
+            btnAbrirPasta.Text = "Abrir Pasta";
+            btnAbrirPasta.UseVisualStyleBackColor = true;
+            // 
+            // rtbInfo
+            // 
+            rtbInfo.BackColor = SystemColors.Info;
+            rtbInfo.Location = new Point(12, 103);
+            rtbInfo.Name = "rtbInfo";
+            rtbInfo.ReadOnly = true;
+            rtbInfo.Size = new Size(597, 277);
+            rtbInfo.TabIndex = 5;
+            rtbInfo.Text = "";
+            // 
+            // ExcutaAutomacaoRenomearArquivos
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(621, 450);
+            Controls.Add(rtbInfo);
+            Controls.Add(btnAbrirPasta);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnExecutar);
+            Controls.Add(label1);
+            Controls.Add(txbPath);
+            Name = "ExcutaAutomacaoRenomearArquivos";
+            Text = "ExcutaAutomacaoRenomearArquivos";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txbPath;
+        private Label label1;
+        private Button btnExecutar;
+        private Button btnCancelar;
+        private Button btnAbrirPasta;
+        private RichTextBox rtbInfo;
+    }
+}
